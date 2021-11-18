c91a133d66582e7ef4b61b8580aba7dd3f1d9ed7
Alsahid Simms <alsahid.simms@version1.com>
2021-11-18 17:29:01 +0000

    Added Student Model

---
+++ src/main/java/com/springApi/api/student/StudentService.java
@@ -0,0 +1,24 @@
+package com.springApi.api.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Benny",
+                        "benny.butcher@gmail.com",
+                        LocalDate.of(2000, Month.AUGUST,5),
+                        21
+                )
+        );
+    }
+}
